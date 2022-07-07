9f37e06fff51f2f6609abfcb13ff466212b543ff
Ricky428 <103875348+Ricky428@users.noreply.github.com>
2022-07-07 12:30:25 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,15 @@ import React from 'react';
 
 export const NavBar = () =>{
     return(
-
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to="/" class="index-link"></NavLink>
+                </li>
+                <li>
+                    <NavLink to="/home"></NavLink>
+                </li>
+            </ul>
+        </nav>
     )
 }
