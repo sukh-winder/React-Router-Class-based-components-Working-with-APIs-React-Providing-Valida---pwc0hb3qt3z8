ca3d6b22ec983a100949791d702c5801aaba8008
Ricky428 <103875348+Ricky428@users.noreply.github.com>
2022-07-16 17:26:25 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,14 +6,8 @@ export const NavBar = () =>{
     return(
         <>
         <nav>
-            <ul>
-                <li>
                     <NavLink to="/" class="index-link">Index</NavLink>
-                </li>
-                <li>
                     <NavLink to="/home" class="home-link">Home</NavLink>
-                </li>
-            </ul>
         </nav>
         </>
     )
