3bfddf1ac6b18491b467be9d3d9ae691c8231cbb
Ricky428 <103875348+Ricky428@users.noreply.github.com>
2022-07-22 13:42:24 +0530

    Update AppRoutes.js

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import {Home} from "./Pages/Home"
 import {Index} from './Pages/Index'
 import {NotFound} from "./Pages/NotFound"
